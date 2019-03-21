90efb56b0819e3211be9f45eb2464dfaca050fc0
Andre K. W <andre.k.willie@gmail.com>
2019-03-21 03:06:38 -0400

    created new route components

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import TokenService from '../services/token';
 
 export function Loader(props) {
   return (
-    <p>Loading...</p>
+    <div className="loader"><p>Loading...</p></div>
   );
+}
+
+export function PrivateRoute({ component, ...props }) {
+  const Component = component
+  return (
+    <Route
+      {...props}
+      render={componentProps => (
+        TokenService.hasAuthToken()
+          ? <Component {...componentProps} />
+          : <Redirect
+              to={{
+                pathname: '/login',
+                state: { from: componentProps.location }
+              }}
+            />
+      )}
+    />
+  )
+}
+
+export function PublicOnlyRoute({ component, ...props }) {
+  const Component = component
+  return (
+    <Route
+      {...props}
+      render={componentProps => (
+        TokenService.hasAuthToken()
+          ? <Redirect to={'/'} />
+          : <Component {...componentProps} />
+      )}
+    />
+  )
 }
