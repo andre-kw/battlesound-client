d2166aff21ddeae4de38d84b5ac36ca14c836cda
Andre K. W <andre.k.willie@gmail.com>
2019-03-29 03:34:26 -0400

    started testing

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom'
+import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 import App from '../components/App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('Renders without crashing', () => {
+  mount(<MemoryRouter><App /></MemoryRouter>);
+});
+
+it('renders as expected when no page is selected', () => {
+  const tree = renderer
+    .create(<MemoryRouter><App /></MemoryRouter>)
+    .toJSON();
+  
+  expect(tree).toMatchSnapshot();  
 });
