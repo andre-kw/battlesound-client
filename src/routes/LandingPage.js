5cf8ca8d95904eb166beb94d4e4fe83f1d5993d6
Andre K. W <andre.k.willie@gmail.com>
2019-05-23 13:31:30 -0400

    styled landing page; added landing text

--- src/routes/LandingPage.js
+++ src/routes/LandingPage.js
@@ -4,7 +4,7 @@ import './LandingPage.css';
 
 export default class LandingPage extends React.Component {
   render() {
-    return (
+    return <>
       <section className="landing">
         {/* <div id="s-bg"></div> */}
         
@@ -14,6 +14,18 @@ export default class LandingPage extends React.Component {
         <Link to="/home" className="btn btn-landing">Enter the battle field</Link>
         <small><Link to="/login">Login</Link> or <Link to="/register">create an account</Link></small>
       </section>
-    );
+
+      <section className="landing-about">
+        <h2>Getting started</h2>
+
+        <p>
+          Whether or not you plan on participate in contests or just voting in them,
+          you can start by creating an account. Any user is able to create a contest 
+          (which will require the approval of the moderator). Once a contest is created, 
+          musicians will be able to send in their submissions and have users vote on 
+          the one they like they most. So go ahead and <a href="/register">get started!</a>
+        </p>
+      </section>
+    </>;
   }
 }
